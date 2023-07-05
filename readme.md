f8c42ee583d2c15a61c2213a14747edf01226937
Ryan D. Knutson <ryanknut@me.com>
2023-07-05 16:48:51 -0500

    did some squealin tbh

---
+++ readme.md
@@ -0,0 +1,7 @@
+# ai_peter clips
+This is intended to host clips from the virtual Family Guy TV show hosted on
+YouTube.
+You can view the live stream on [aipeter.live](https://aipeter.live/)
+
+Be sure to join the [Discord](https://discord.gg/aipeter) to suggest topics,
+chat, and more!
--- src/create-db.sql
+++ src/create-db.sql
@@ -1,17 +1,26 @@
 --- PostgreSQL
 
 CREATE TABLE videos (
-  videoid SERIAL PRIMARY KEY,
+  videoid SMALLSERIAL PRIMARY KEY,
   title VARCHAR(255),
   filename VARCHAR(255) UNIQUE,
-  uploaded TIMESTAMP NOT NULL DEFAULT current_timestamp
+  uploaded TIMESTAMP NOT NULL DEFAULT NOW(),
+  userid SMALLINT,
+  likes SMALLINT 
 )
   
-
-
 CREATE TABLE users (
-  userid SERIAL PRIMARY KEY,
+  userid SMALLSERIAL PRIMARY KEY,
   username VARCHAR(20) UNIQUE,
   email VARCHAR(255),
+  pw BINARY(60),
   bio TEXT
 );
+
+CREATE TABLE comments (
+  commentid SERIAL PRIMARY KEY,
+  userid SMALLINT,
+  data TEXT,
+  likes SMALLINT,
+  posted TIMESTAMP NOT NULL DEFAULT NOW()
+); 
