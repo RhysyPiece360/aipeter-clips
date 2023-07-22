ccbd10146b9999f727e97a90092a4ff6827ec48f
Ryan Knutson <ryanknut@me.com>
2023-07-22 04:49:47 -0500

    updated sql, added dummy data

--- server.js
+++ server.js
@@ -7,8 +7,36 @@ const port = 3420
 app.set('view engine', 'pug')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
+let sqlDriver = {
+  postLimit: 10,
+  homepagePostLimit: 6,
+  homepagePosts: function() {
+    console.log('this will get homepage posts')
+    return `SELECT *
+    FROM videos
+    ORDER BY uploaded DESC
+    LIMIT ${this.homepagePostLimit};`
+  },
+  // user pages, accepts both username and userid (for permalink)
+  userPage: function(userid) {
+    console.log('this will get the info needed for user page')
+    return `PREPARE userPage (int) AS
+    SELECT userid, username, discordusername, created, bio
+    FROM users
+    WHERE userid = $1;
+    EXECUTE userPage(${userid});`
+  },
+  aiShowPage: function(show) {
+    console.log('this will get videos for `show`')
+    
+
+  }
+}
+
 
 app.get('/', (req, res) => {
+  console.log(sqlDriver.userPage('bingus123'))
+  console.log(sqlDriver.userPage('123'))
   res.render('index', { title: 'wabangus tbh' })
 })
 
--- src/create-db.sql
+++ src/create-db.sql
@@ -1,37 +1,40 @@
 --- PostgreSQL
 
+CREATE TABLE users (
+  userid SERIAL PRIMARY KEY,
+  username VARCHAR(20) UNIQUE,
+  email VARCHAR(254) UNIQUE NOT NULL,
+  pw CHAR(60) NOT NULL,
+  created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  discordusername VARCHAR(37),
+  bio TEXT
+);
+
 CREATE TABLE videos (
   videoid SERIAL PRIMARY KEY,
   channel VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   filename VARCHAR(255) UNIQUE NOT NULL,
   uploaded TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  userid INT NOT NULL
-)
-
-CREATE TABLE videolikes (
-  videoid INT NOT NULL,
-  userid INT DEFAULT NULL
-)
-  
-CREATE TABLE users (
-  userid SERIAL PRIMARY KEY,
-  username VARCHAR(20) UNIQUE,
-  email VARCHAR(254) UNIQUE NOT NULL,
-  pw BINARY(60) NOT NULL,
-  created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  bio TEXT
+  userid INT NOT NULL REFERENCES users (userid),
+  likes INT DEFAULT 0
 );
 
 CREATE TABLE comments (
   commentid SERIAL PRIMARY KEY,
-  videoid INT NOT NULL,
+  videoid INT REFERENCES videos (videoid),
+  userid INT REFERENCES users (userid),
   text TEXT NOT NULL,
-  userid INT NOT NULL,
-  posted TIMESTAMPTZ NOT NULL DEFAULT NOW()
-); 
+  posted TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  likes INT DEFAULT 0
+);
 
-CREATE TABLE commentlikes (
-  commentid INT NOT NULL,
-  userid SMALLINT DEFAULT NULL
-)
+/* CREATE TABLE videolikes ( */
+/*   videoid INT REFERENCES videos(videoid) NOT NULL, */
+/*   userid INT DEFAULT NULL */
+/* ) */
+
+/* CREATE TABLE commentlikes ( */
+/*   commentid INT REFERENCES comments(commentid) NOT NULL, */
+/*   userid INT DEFAULT NULL */
+/* ) */
