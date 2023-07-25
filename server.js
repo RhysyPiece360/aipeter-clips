9e0f96649b89177dec8dccaaa4757035310277c1
RhysyPiece360 <67196752+RhysyPiece360@users.noreply.github.com>
2023-07-25 14:16:01 -0400

    seperate the funny list to its own file for readability and chore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('node:path')
 const SITEURL = new URL('http://10.0.0.187:3420')
 
 const crypto = require('node:crypto')
-
+const list = require('./list');
 // set express stuffs tbh
 const express = require('express')
 const app = express()
@@ -18,45 +18,7 @@ app.use('/api/video', express.static(VIDEOSDIR))
 // .env config
 require('dotenv').config()
 console.log(process.env)
-
 const poweredBy = () => {
-  const list = [
-    'sex on tv',
-    'child slave labor',
-    'cum',
-    'your parent\'s failed marriage',
-    'SAMSUNG',
-    'a hamster on one of those spinny wheel things',
-    'Procrastination™',
-    'Walter White',
-    'Walter White\'s meth',
-    '\'*\'',
-    'hot babes in your area!',
-    'aliens',
-    'why do I exist juice',
-    'Owner\'s cat',
-    'erotic furry RP',
-    'the AIDS virus',
-    'spooky dark unknown forces of evil',
-    'Brawndo (it\'s got what plants crave)',
-    'PHP',
-    'the Planet Express crew',
-    'the children locked in my basement',
-    'the Brainfuck programming language',
-    'fondy',
-    'Segmentation fault (core dumped)',
-    'the Metaverse',
-    'MoistCr1TiKaL (aka Jesus)',
-    'my sentient artificial intelligence friend named Steve',
-    'bad code',
-    'goblins',
-    'LSD',
-    'Columbia Coke',
-    'Bryan Cranstons Testicles',
-    'The Tonight Show starring Jimmy Fallon',
-    'the CCP'
-  ]
-
   return list[crypto.randomBytes(4).readUInt32BE() % list.length]
 }
 
